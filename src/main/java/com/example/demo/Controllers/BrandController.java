a42055ca62bd80ccd483c6412c7131505b98b802
juan duran <juanmanuelduranrueda@gmail.com>
2023-03-09 17:16:26 -0500

    correcting and implementing the restcontroller notation. Updating the database connection

--- src/main/java/com/example/demo/Controllers/BrandController.java
+++ src/main/java/com/example/demo/Controllers/BrandController.java
@@ -1,17 +1,26 @@
 package com.example.demo.Controllers;
 
 import com.example.demo.Entities.Brand;
+import com.example.demo.Entities.City;
 import com.example.demo.Services.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+
+@RestController
 public class BrandController {
 
     @Autowired
     BrandService brands;
+    @GetMapping(value="/all")
+    public ArrayList<Brand> getAll(){
+        return (ArrayList<Brand>) brands.SearchAll();
+    }
 
     @PostMapping(value="/NewBrand")
     public void NewBrand(@RequestBody Brand brand){
--- src/main/java/com/example/demo/Controllers/CityController.java
+++ src/main/java/com/example/demo/Controllers/CityController.java
@@ -1,8 +1,35 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Entities.City;
+import com.example.demo.Entities.Tool;
+import com.example.demo.Services.CityService;
+import com.example.demo.Services.ToolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+
+@RestController
 public class CityController {
+    @Autowired
+    CityService city;
+
+
+
+    @GetMapping(value="/all")
+    public ArrayList<City> getAll(){
+        return (ArrayList<City>) city.SearchAll();
+    }
+
+
+
+    @PostMapping(value="/NewCity")
+    public void NewCity(@RequestBody City city1){
+        city.InsertNewTool(city1);
+    }
 
 }
