2480000b95605fae490d31e8f49584e72ab7a947
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2023-03-08 20:20:20 -0500

    Actualization of classes

--- src/main/java/com/example/demo/Repositories/CityRepository.java
+++ src/main/java/com/example/demo/Repositories/CityRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.Repositories;
 
-import com.example.demo.Entities.Brand;
 import com.example.demo.Entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.ArrayList;
 
-public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
+public interface CityRepository extends JpaRepository<City, Integer> {
 
 }
---
+++ src/main/java/com/example/demo/Services/CityService.java
@@ -0,0 +1,27 @@
+package com.example.demo.Services;
+
+import com.example.demo.Entities.City;
+import com.example.demo.Entities.Tool;
+import com.example.demo.Repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CityService {
+    @Autowired
+    CityRepository Repo;
+
+    public void InsertNewTool(City c){
+        Repo.save(c);
+    }
+
+    public ArrayList<City> SearchAll(){
+        return (ArrayList<City>) Repo.findAll();
+    }
+
+    public void Put(City c){
+                
+    }
+}
