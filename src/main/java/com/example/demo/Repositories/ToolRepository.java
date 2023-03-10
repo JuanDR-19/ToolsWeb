758ba3e9c6171f8bbc4c0851041a33b4eca06164
juan duran <juanmanuelduranrueda@gmail.com>
2023-03-09 20:27:35 -0500

    .

--- src/main/java/com/example/demo/Repositories/ToolRepository.java
+++ src/main/java/com/example/demo/Repositories/ToolRepository.java
@@ -17,5 +17,4 @@ public interface ToolRepository extends JpaRepository<Tool, Integer> {
     @Modifying //update
     @Query("UPDATE Tool t SET t.name = :name, t.img = :img, t.description = :description, t.brand = :brand, t.price = :price, t.cities = :cities, t.quantity = :quantity WHERE t.id = :id")
     void actualizarTool(@Param("name") String name, @Param("img") URL img, @Param("description") String description, @Param("brand") Brand brand, @Param("price") double price, @Param("cities") ArrayList<City> cities, @Param("quantity") Integer quantity, @Param("id") Integer id);
-    ArrayList<Tool> findAll();
 }
